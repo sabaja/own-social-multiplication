6fe9fc9a78f1d97e082b976a943adbc47b26f31f
sabaja <jacoposabatini76@gmail.com>
2018-10-18 23:32:45 +0200

    Modifica MultiplicationResultAttempt terminata - continua a pag66

--- src/test/java/microservice/book/socialmultiplication/controller/MultiplicationResultAttemptControllerTest.java
+++ src/test/java/microservice/book/socialmultiplication/controller/MultiplicationResultAttemptControllerTest.java
@@ -20,64 +20,63 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import microservice.book.socialmultiplication.controller.MultiplicationResultAttemptController.ResultResponse;
 import microservice.book.socialmultiplication.domain.Multiplication;
 import microservice.book.socialmultiplication.domain.MultiplicationResultAttempt;
 import microservice.book.socialmultiplication.domain.User;
 import microservice.book.socialmultiplication.service.MultiplicationService;
 
-
 @RunWith(SpringRunner.class)
 @WebMvcTest(MultiplicationResultAttemptController.class)
 public class MultiplicationResultAttemptControllerTest {
-	
+
 	@MockBean
 	private MultiplicationService multiplicationService;
-	
+
 	@Autowired
 	private MockMvc mvc;
-	@Autowired
-	private MultiplicationResultAttemptController controller; 
 
-    // These objects will be magically initialized by the initFields method below.
+	// These objects will be magically initialized by the initFields method below.
 	private JacksonTester<MultiplicationResultAttempt> jsonResult;
-	private JacksonTester<ResultResponse> jsonResponse;
-	
+	private JacksonTester<MultiplicationResultAttempt> jsonResponse;
+
 	@Before
 	public void setup() {
 		JacksonTester.initFields(this, new ObjectMapper());
 	}
-	
+
 	@Test
 	public void checkCorrectAttempt() throws Exception {
 		this.genericParameterizedTest(true);
 	}
-	
+
 	@Test
 	public void checkWrongAttempt() throws Exception {
 		this.genericParameterizedTest(false);
 	}
-	
-	//Simula una chiamata http e ne verifica lo stato che sia 200
-	private void genericParameterizedTest(final boolean correct)throws Exception {
+
+	// Simula una chiamata http e ne verifica lo stato che sia 200
+	private void genericParameterizedTest(final boolean correct) throws Exception {
 		given(multiplicationService.checkAttempt(any(MultiplicationResultAttempt.class))).willReturn(correct);
-		  User user = new User("john");
-	        Multiplication multiplication = new Multiplication(50, 70);
-	        MultiplicationResultAttempt attempt = null;
-	        attempt = correct ?  new MultiplicationResultAttempt(multiplication, user, 3500, true) : new MultiplicationResultAttempt(multiplication, user, 350, false); 
-	        
+		User user = new User("john");
+		Multiplication multiplication = new Multiplication(50, 70);
+		MultiplicationResultAttempt attempt = null;
+		attempt = correct ? new MultiplicationResultAttempt(multiplication, user, 3500, true)
+				: new MultiplicationResultAttempt(multiplication, user, 350, false);
 
-	        // when
-	        MockHttpServletResponse response = mvc.perform(
-	                post("/results").contentType(MediaType.APPLICATION_JSON)
-	                        .content(jsonResult.write(attempt).getJson()))
-	                .andReturn().getResponse();
+		// when
+		MockHttpServletResponse response = mvc.perform(
+				post("/results").contentType(MediaType.APPLICATION_JSON).content(jsonResult.write(attempt).getJson()))
+				.andReturn().getResponse();
 
-	        // then
-	        assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
-	        assertThat(response.getContentAsString()).isEqualTo(
-	                jsonResponse.write(controller.new ResultResponse(correct, 3500)).getJson());
-	        
-		
+		// then
+		assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+		assertThat(
+				response.getContentAsString())
+						.isEqualTo(
+								jsonResponse
+										.write(new MultiplicationResultAttempt(attempt.getMultiplication(),
+												attempt.getUser(), attempt.getResultAttempt(), attempt.isCorrect()))
+										.getJson());
+
 	}
 }
