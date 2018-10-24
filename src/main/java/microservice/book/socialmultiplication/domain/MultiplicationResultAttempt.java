7d64baf6e916907d7f4ffcbd0da4e3a692a361e4
sabaja <jacoposabatini76@gmail.com>
2018-10-24 23:27:17 +0200

    ADD Entity fashion

--- src/main/java/microservice/book/socialmultiplication/domain/MultiplicationResultAttempt.java
+++ src/main/java/microservice/book/socialmultiplication/domain/MultiplicationResultAttempt.java
@@ -1,5 +1,12 @@
 package microservice.book.socialmultiplication.domain;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -13,9 +20,21 @@ import lombok.ToString;
 @EqualsAndHashCode
 public class MultiplicationResultAttempt {
 
+	@Id
+	@GeneratedValue
+	@Column(name = "ID", nullable = false, unique = true)
+	private Long id; 
+	
+	@ManyToOne(cascade = CascadeType.PERSIST)
+	@JoinColumn(name = "MULTIPLICATION_ID")
 	private final Multiplication multiplication;
+	
+	@ManyToOne(cascade = CascadeType.PERSIST)
+	@JoinColumn(name = "USER_ID")
 	private final User user;
+	@Column(name = "RESULT_ATTEMPT")
 	private final int resultAttempt;
+	@Column(name = "IS_CORRECT")
 	private final boolean correct;
 
 	public MultiplicationResultAttempt() {
--- src/main/java/microservice/book/socialmultiplication/domain/User.java
+++ src/main/java/microservice/book/socialmultiplication/domain/User.java
@@ -1,5 +1,11 @@
 package microservice.book.socialmultiplication.domain;
 
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -11,8 +17,19 @@ import lombok.ToString;
 @Setter
 @EqualsAndHashCode
 @ToString
-public class User {
+public class User implements Serializable{
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1490401593315076434L;
+
+	@Id
+	@GeneratedValue
+	@Column(name = "USER_ID", nullable = false, unique = true)
+	private Long id; 
+	
+	@Column(name = "ALIAS", unique = true, nullable = false, length = 150)
 	private final String alias;
 
 	public User() {
