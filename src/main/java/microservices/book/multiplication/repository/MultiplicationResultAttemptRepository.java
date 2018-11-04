a36c6448fbcb01c4ced2b7a8a5fe31e156519663
sabaja <jacoposabatini76@gmail.com>
2018-11-04 16:27:31 +0100

    Migliorata wrong check per MultiplicationServiceImplTest

--- src/main/java/microservices/book/multiplication/repository/MultiplicationResultAttemptRepository.java
+++ src/main/java/microservices/book/multiplication/repository/MultiplicationResultAttemptRepository.java
@@ -2,11 +2,11 @@ package microservices.book.multiplication.repository;
 
 import java.util.List;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import microservices.book.multiplication.domain.MultiplicationResultAttempt;
 
-public interface MultiplicationResultAttemptRepository extends CrudRepository<MultiplicationResultAttempt, Long> {
+public interface MultiplicationResultAttemptRepository extends JpaRepository<MultiplicationResultAttempt, Long> {
 
     /**
      * @return the latest 5 attempts for a given user, identified by their alias.
--- src/test/java/microservices/book/multiplication/service/MultiplicationServiceImplTest.java
+++ src/test/java/microservices/book/multiplication/service/MultiplicationServiceImplTest.java
@@ -20,92 +20,94 @@ import static org.mockito.Mockito.verify;
 
 public class MultiplicationServiceImplTest {
 
-    private MultiplicationServiceImpl multiplicationServiceImpl;
+	private MultiplicationServiceImpl multiplicationServiceImpl;
 
-    @Mock
-    private RandomGeneratorService randomGeneratorService;
+	@Mock
+	private RandomGeneratorService randomGeneratorService;
 
-    @Mock
-    private MultiplicationResultAttemptRepository attemptRepository;
+	@Mock
+	private MultiplicationResultAttemptRepository attemptRepository;
 
-    @Mock
-    private UserRepository userRepository;
+	@Mock
+	private UserRepository userRepository;
 
-    @Before
-    public void setUp() {
-        // With this call to initMocks we tell Mockito to process the annotations
-        MockitoAnnotations.initMocks(this);
-        multiplicationServiceImpl = new MultiplicationServiceImpl(randomGeneratorService, attemptRepository, userRepository);
-    }
+	@Before
+	public void setUp() {
+		// With this call to initMocks we tell Mockito to process the annotations
+		MockitoAnnotations.initMocks(this);
+		multiplicationServiceImpl = new MultiplicationServiceImpl(randomGeneratorService, attemptRepository,
+				userRepository);
+	}
 
-    @Test
-    public void createRandomMultiplicationTest() {
-        // given (our mocked Random Generator service will return first 50, then 30)
-        given(randomGeneratorService.generateRandomFactor()).willReturn(50, 30);
+	@Test
+	public void createRandomMultiplicationTest() {
+		// given (our mocked Random Generator service will return first 50, then 30)
+		given(randomGeneratorService.generateRandomFactor()).willReturn(50, 30);
 
-        // when
-        Multiplication multiplication = multiplicationServiceImpl.createRandomMultiplication();
+		// when
+		Multiplication multiplication = multiplicationServiceImpl.createRandomMultiplication();
 
-        // then
-        assertThat(multiplication.getFactorA()).isEqualTo(50);
-        assertThat(multiplication.getFactorB()).isEqualTo(30);
-    }
+		// then
+		assertThat(multiplication.getFactorA()).isEqualTo(50);
+		assertThat(multiplication.getFactorB()).isEqualTo(30);
+	}
 
-    @Test
-    public void checkCorrectAttemptTest() {
-        // given
-        Multiplication multiplication = new Multiplication(50, 60);
-        User user = new User("john_doe");
-        MultiplicationResultAttempt attempt = new MultiplicationResultAttempt(
-                user, multiplication, 3000, false);
-        MultiplicationResultAttempt verifiedAttempt = new MultiplicationResultAttempt(
-                user, multiplication, 3000, true);
-        given(userRepository.findByAlias("john_doe")).willReturn(Optional.empty());
+	@Test
+	public void checkCorrectAttemptTest() {
+		// given
+		Multiplication multiplication = new Multiplication(50, 60);
+		User user = new User("john_doe");
+		// E' una simulazione della risposta da internet, prima di essere processata
+		// dalla
+		// request post /results la variabile isCorrect è false
+		MultiplicationResultAttempt attempt = new MultiplicationResultAttempt(user, multiplication, 3000, false);
 
-        // when
-        boolean attemptResult = multiplicationServiceImpl.checkAttempt(attempt);
+		// La request è processata dal controller MultiplicationResultAttemptController
+		MultiplicationResultAttempt verifiedAttempt = new MultiplicationResultAttempt(user, multiplication, 3000, true);
 
-        // then
-        assertThat(attemptResult).isTrue();
-        verify(attemptRepository).save(verifiedAttempt);
-    }
+		given(userRepository.findByAlias("john_doe")).willReturn(Optional.empty());
 
-    @Test
-    public void checkWrongAttemptTest() {
-        // given
-        Multiplication multiplication = new Multiplication(50, 60);
-        User user = new User("john_doe");
-        MultiplicationResultAttempt attempt = new MultiplicationResultAttempt(
-                user, multiplication, 3010, false);
-        given(userRepository.findByAlias("john_doe")).willReturn(Optional.empty());
+		// when
+		boolean attemptResult = multiplicationServiceImpl.checkAttempt(attempt);
 
-        // when
-        boolean attemptResult = multiplicationServiceImpl.checkAttempt(attempt);
+		// then
+		assertThat(attemptResult).isTrue();
+		verify(attemptRepository).save(verifiedAttempt);
+	}
 
-        // then
-        assertThat(attemptResult).isFalse();
-        verify(attemptRepository).save(attempt);
-    }
+	@Test
+	public void checkWrongAttemptTest() {
+		// given
+		Multiplication multiplication = new Multiplication(50, 60);
+		User user = new User("john_doe");
+		MultiplicationResultAttempt attempt = new MultiplicationResultAttempt(user, multiplication, 3010, false);
+		MultiplicationResultAttempt verifiedAttempt = new MultiplicationResultAttempt(user, multiplication, 3010, false);
+		
+		given(userRepository.findByAlias("john_doe")).willReturn(Optional.empty());
 
-    @Test
-    public void retrieveStatsTest() {
-        // given
-        Multiplication multiplication = new Multiplication(50, 60);
-        User user = new User("john_doe");
-        MultiplicationResultAttempt attempt1 = new MultiplicationResultAttempt(
-                user, multiplication, 3010, false);
-        MultiplicationResultAttempt attempt2 = new MultiplicationResultAttempt(
-                user, multiplication, 3051, false);
-        List<MultiplicationResultAttempt> latestAttempts = Lists.newArrayList(attempt1, attempt2);
-        given(userRepository.findByAlias("john_doe")).willReturn(Optional.empty());
-        given(attemptRepository.findTop5ByUserAliasOrderByIdDesc("john_doe"))
-                .willReturn(latestAttempts);
+		// when
+		boolean attemptResult = multiplicationServiceImpl.checkAttempt(attempt);
 
-        // when
-        List<MultiplicationResultAttempt> latestAttemptsResult =
-                multiplicationServiceImpl.getStatsForUser("john_doe");
+		// then
+		assertThat(attemptResult).isFalse();
+		verify(attemptRepository).save(verifiedAttempt);
+	}
 
-        // then
-        assertThat(latestAttemptsResult).isEqualTo(latestAttempts);
-    }
+	@Test
+	public void retrieveStatsTest() {
+		// given
+		Multiplication multiplication = new Multiplication(50, 60);
+		User user = new User("john_doe");
+		MultiplicationResultAttempt attempt1 = new MultiplicationResultAttempt(user, multiplication, 3010, false);
+		MultiplicationResultAttempt attempt2 = new MultiplicationResultAttempt(user, multiplication, 3051, false);
+		List<MultiplicationResultAttempt> latestAttempts = Lists.newArrayList(attempt1, attempt2);
+		given(userRepository.findByAlias("john_doe")).willReturn(Optional.empty());
+		given(attemptRepository.findTop5ByUserAliasOrderByIdDesc("john_doe")).willReturn(latestAttempts);
+
+		// when
+		List<MultiplicationResultAttempt> latestAttemptsResult = multiplicationServiceImpl.getStatsForUser("john_doe");
+
+		// then
+		assertThat(latestAttemptsResult).isEqualTo(latestAttempts);
+	}
 }
