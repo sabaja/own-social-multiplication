26927d383adfd3d4ffa9905b147c1f2f104b87f0
sabaja <jacoposabatini76@gmail.com>
2018-11-24 17:43:41 +0100

    "/results/{resultId}" endpoint added

--- src/main/java/microservices/book/multiplication/controller/MultiplicationResultAttemptController.java
+++ src/main/java/microservices/book/multiplication/controller/MultiplicationResultAttemptController.java
@@ -8,6 +8,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -56,5 +57,10 @@ final class MultiplicationResultAttemptController {
 	@GetMapping
 	ResponseEntity<List<MultiplicationResultAttempt>> getStatistics(@RequestParam("alias") String alias) {
 		return ResponseEntity.ok(multiplicationService.getStatsForUser(alias));
+	}
+
+	@GetMapping("/{resultId}")
+	public ResponseEntity<MultiplicationResultAttempt> getResultById(@PathVariable("resultId") Long resultId) {
+		return ResponseEntity.ok(multiplicationService.getResultById(resultId));
 	}
 }
--- src/main/java/microservices/book/multiplication/service/MultiplicationService.java
+++ src/main/java/microservices/book/multiplication/service/MultiplicationService.java
@@ -5,28 +5,39 @@ import microservices.book.multiplication.domain.MultiplicationResultAttempt;
 
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 public interface MultiplicationService {
 
-    /**
-     * Creates a Multiplication object with two randomly-generated factors
-     * between 11 and 99.
-     *
-     * @return a Multiplication object with random factors
-     */
-    Multiplication createRandomMultiplication();
+	/**
+	 * Creates a Multiplication object with two randomly-generated factors between
+	 * 11 and 99.
+	 *
+	 * @return a Multiplication object with random factors
+	 */
+	Multiplication createRandomMultiplication();
 
-    /**
-     * @return true if the attempt matches the result of the
-     *         multiplication, false otherwise.
-     */
-    boolean checkAttempt(final MultiplicationResultAttempt resultAttempt);
+	/**
+	 * @return true if the attempt matches the result of the multiplication, false
+	 *         otherwise.
+	 */
+	boolean checkAttempt(final MultiplicationResultAttempt resultAttempt);
 
-    /**
-     * Gets the statistics for a given user.
-     *
-     * @param userAlias the user's alias
-     * @return a list of {@link MultiplicationResultAttempt} objects, being the past attempts of the user.
-     */
-    List<MultiplicationResultAttempt> getStatsForUser(final String userAlias);
+	/**
+	 * Gets the statistics for a given user.
+	 *
+	 * @param userAlias the user's alias
+	 * @return a list of {@link MultiplicationResultAttempt} objects, being the past
+	 *         attempts of the user.
+	 */
+	List<MultiplicationResultAttempt> getStatsForUser(final String userAlias);
 
+	/**
+	 * 
+	 * @param resultId of {@link MultiplicationResultAttempt}
+	 * @return a {@link MultiplicationResultAttempt}
+	 */
+	MultiplicationResultAttempt getResultById(Long resultId);
 }
