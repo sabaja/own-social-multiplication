1a7221f8806e8027d4ce41198ee66638d902212e
sabaja <jacoposabatini76@gmail.com>
2019-02-03 18:22:45 +0100

    Change server url - API Gateway Integration

--- src/main/java/microservices/book/multiplication/configuration/RabbitMQConfiguration.java
+++ src/main/java/microservices/book/multiplication/configuration/RabbitMQConfiguration.java
@@ -1,8 +1,5 @@
 package microservices.book.multiplication.configuration;
 
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
