1577eac972511b5b1818318620726af9208df026
sabaja <jacoposabatini76@gmail.com>
2019-02-08 17:03:33 +0100

    Bug fixing - Service Registration failed

--- src/main/java/microservices/book/SocialMultiplicationApplication.java
+++ src/main/java/microservices/book/SocialMultiplicationApplication.java
@@ -4,9 +4,11 @@ import java.io.IOException;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
+@EnableEurekaClient
 //@ComponentScan("microservice.book")
 //@Configuration
 //@EnableAutoConfiguration
